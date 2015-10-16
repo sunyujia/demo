5c1808b5b104054c1874f69b38dea4b6dae0735f
linzhe0501 <linzhe0501@163.com>
2015-10-16 20:32:35 +0800

    x

--- XianLifeDemo/app/src/main/java/com/jiandanlicai/xianlifedemo/YzhFragment.java
+++ XianLifeDemo/app/src/main/java/com/jiandanlicai/xianlifedemo/YzhFragment.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class YzhFragment extends Fragment implements View.OnClickListener {
-
+    //我
     private OnViewClickListener mCallback;
 
     private boolean mIsLogin;
